1e89d40b99dfcbefca3aa3d7cc114f83cb876cb5
BrodieAdnan <adnangazi.ag@gmail.com>
2020-03-24 16:51:30 +0100

    Commenti aggiunti

--- Programmazione 1/StampaFetta/main.cpp
+++ Programmazione 1/StampaFetta/main.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 using namespace std;
 
+//PRE=(x è array tridimensionale di int [strati][4][5] completamente definito, fetta è int >= 0 e <= 3)
 void stampaH (int x[][4][5], int strati, int fetta) {
-    for (int i = 0; i < strati; ++i) {
-        for (int j = 0; j < 5; ++j) {
+    for (int i = 0; i < strati; ++i) { //R1
+        for (int j = 0; j < 5; ++j) { //R2
             cout << x[i][fetta][j] << ' ';
-        }
+        } //POST-R2
         cout << endl;
-    }
+    } //POST-R1
 }
+//POST=(stampa la riga fetta in x)
 
+//PRE=(x è array tridimensionale di int [strati][4][5] completamente definito, fetta è int >= 0 e <= 4)
 void stampaV (int x[][4][5], int strati, int fetta) {
     for (int i = 0; i < strati; ++i) {
         for (int j = 0; j < 4; ++j) {
@@ -18,6 +21,7 @@ void stampaV (int x[][4][5], int strati, int fetta) {
         cout << endl;
     }
 }
+//POST=(stampa la colonna fetta in x)
 
 int main() {
   int A[3][4][5];
@@ -47,3 +51,20 @@ int main() {
 
   cout<<"end"<<endl;
 }
+
+/*
+ * R1=(0 <= i <= strati) && (itera negli strati di x)
+ * POST-R1=(iterato tutti gli strati di x)
+ *
+ * R2=(0 <= j <= 5) && (itera nelle colonne di x) && (stampa tutte le colonne della riga fetta)
+ * POST-R2=(iterato tutte le colonne di x && stampato riga fetta di x)
+ *
+ * Prova di correttezza di stampaH:
+ * 1. Vengono iterati tutti gli strati di x in modo da stampare la riga fetta per ogni strato => (0 <= i < strati).
+ * 2. Ad ogni iterazione degli strati vengono iterate tutte le colonne di x => (0 <= j < 5).
+ * 3. Per ogni strato vengono quindi stampati tutti gli elementi della riga fetta in x: gli elementi della
+ * riga fetta sono le colonne di x in cui vengono tenute fissate la riga fetta da stampare.
+ * 4. Terminato di stampare una riga il ciclo interno che scorre le colonne termina perchè una riga ha lo stesso numero di elementi
+ * del numero delle colonne, e si passa allo strato successivo.
+ * 6. Terminato di scorrere gli strati, tutte le righe sono state stampate, e quindi si esce dal ciclo esterno e la  funzione termina.
+ */
--- Programmazione 1/StampaFettaParziale/main.cpp
+++ Programmazione 1/StampaFettaParziale/main.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 
+//PRE=(x array tridimensionale di int [3][4][5] definito solo nelle prime n_ele posizioni, fetta int >= 0 e <= 4)
 void stampaH (int x[][4][5], int n_ele, int fetta) {
     int k, s; //k contiene il numero di elementi definiti nella riga da stampare
     for (int i = 0; i < 3; ++i) {
         s = 20 * i;
-        if (n_ele - (fetta*5 + s) >= 5) {
+        if (n_ele - (fetta*5 + s) >= 5) { //tutta la riga è definita
             k = 5;
-        } else {
+        } else { //definita parzialmente o non definita
             k = n_ele - (fetta*5 + s);
         }
 
@@ -18,13 +19,15 @@ void stampaH (int x[][4][5], int n_ele, int fetta) {
             for (int j = 0; j < k; ++j) {
                 cout << x[i][fetta][j] << ' ';
             }
+            cout << endl;
         }
-        cout << endl;
     }
 }
+//POST=(stampa solo gli elementi definiti della riga fetta di x)
 
+//PRE=(x array tridimensionale di int [3][4][5] definito solo nelle prime n_ele posizioni, fetta int >= 0 e <= 5)
 void stampaV (int x[][4][5], int n_ele, int fetta) {
-    int k, s;
+    int s;
     for (int i = 0; i < 3; ++i) {
         s = 20 * i;
         if (n_ele <= fetta + s) { //colonna da stampare completamente vuota
@@ -32,14 +35,15 @@ void stampaV (int x[][4][5], int n_ele, int fetta) {
             return;
         } else { //colonna da stampare completamente o parzialmente definita
             for (int j = 0; j < 4; ++j) {
-                if (n_ele > s + fetta + 5*j) {
+                if (n_ele > s + fetta + 5*j) { //elemento della colonna è definito
                     cout << x[i][j][fetta] << ' ';
                 }
             }
+            cout << endl;
         }
-        cout << endl;
     }
 }
+//POST=(stampa solo gli elementi definiti della colonna fetta di x)
 
 int main() {
   cout<<"start"<<endl;
@@ -66,3 +70,13 @@ int main() {
     cout<<"end"<<endl;
 }
 
+/*
+ * Prova di correttezza di stampaH:
+ * 1. Vengono iterati tutti gli strati di x in modo da stampare la riga fetta per ogni strato => (0 <= i < strati).
+ * 2. Ad ogni iterazione degli strati vengono iterate tutte le colonne di x => (0 <= j < 5).
+ * 3. Per ogni strato vengono quindi stampati tutti gli elementi della riga fetta in x: gli elementi della
+ * riga fetta sono le colonne di x in cui vengono tenute fissate la riga fetta da stampare.
+ * 4. Terminato di stampare una riga il ciclo interno che scorre le colonne termina perchè una riga ha lo stesso numero di elementi
+ * del numero delle colonne, e si passa allo strato successivo.
+ * 6. Terminato di scorrere gli strati, tutte le righe sono state stampate, e quindi si esce dal ciclo esterno e la  funzione termina.
+ */
