ff93dbfb2e2a717c41ca91460ab34f93e844c974
BrodieAdnan <adnangazi.ag@gmail.com>
2020-05-03 23:14:33 +0200

    Prova induttiva ben fatta

--- Programmazione 1/StringaPalindroma/main.cpp
+++ Programmazione 1/StringaPalindroma/main.cpp
@@ -25,3 +25,33 @@ int main() {
         cout << "la stringa non e' palindroma" << endl;
     }
 }
+
+/********************************************************************************************************************************************************************
+    Prova induttiva della funzione 'palindroma(char * S, int n)':
+	La correttezza di questa funzione ricorsiva (che chiameremo F) è dimostrata eseguendo un'ipotesi induttiva sulla funzione
+    richiamata internamente, ovvero la stessa funzione 'palindroma(char * S, int n)' (che chiameremo G).
+
+	G riceve come parametri formali i parametri attuali alla chiamata da F, ovvero:
+		- S + 1: array di caratteri di lunghezza >= 0, in quanto la chiamata ricorsiva avviene sse la lunghezza si S > 3 ==> S + 1
+		è un array di dimensione positiva
+		- n - 2: intero >= 0, in quanto la chiamata ricorsiva avviene sse n > 3 ==> n - 2 >= 0
+	==> quindi G rispetta il PRE-condizione della funzione.
+
+	Entrambi i casi base di G rientrano nella definizione del POST-condizione di G:
+		- caso base 1: return true sse stringa palindroma in tutti gli elementi
+		- caso base 2: return false sse stringa non palindroma ad un quanlunque elemento.
+	==> presumendo la corretteza del caso ricorsivo in G (che avviene sse n > 3), abbiamo che G rispetta il POST-condizione della funzione.
+
+	Usando la correttezza di G, dimostro la correttezza di F:
+		In F, se la lunghezza della stringa è <= 3, si passa direttamente ai casi base:
+		- caso base 1: return true sse il primo e l'ultimo elemento sono uguali:
+			- se la stringa ha dimensione 1, il primo e l'ultimo elemento coincidono, e quindi sono uguali ==> palindromo.
+			- se la striga ha dimensione 2, la stringa è palindroma se il primo elemento coincide con il secondo (anche ultimo) elemento.
+			- se la striga ha dimensione 3, la stringa è palindroma se il primo elemento coincide con l'ultimo elemento ==> l'unico elemento
+			in mezzo è palindromo perchè di dimensione 1.
+		- caso base 2: return false sse il caso base 1 non è verificato ==> stringa di dimensione 2 0 3 con primo e ultimo elemento diversi.
+		- altrimenti si effttua lo stesso processo in una chiamata alla stessa funzione, in cui si considera una stringa con 2 elementi in meno (i due agli estremi).
+
+	==> dimostro così la corretteza della funzione F, e quindi anche di G. Viene dimostrato così la correttezza della funzione 'palindroma(char * S, int n)'
+    rispetto al suo PRE e POST condizione.
+ ********************************************************************************************************************************************************************/
