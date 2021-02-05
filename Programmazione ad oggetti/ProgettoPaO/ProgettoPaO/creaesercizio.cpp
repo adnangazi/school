27aadd25a58db4a41540e0228e51450b91d02531
BrodieAdnan <adnangazi.ag@gmail.com>
2021-02-05 13:25:28 +0100

    Aggiustamenti classe CreaEsercizio
    Static le funzioni della classe CreaEsercizio.

--- Programmazione ad oggetti/ProgettoPaO/ProgettoPaO/creaesercizio.cpp
+++ Programmazione ad oggetti/ProgettoPaO/ProgettoPaO/creaesercizio.cpp
@@ -1,21 +1,21 @@
 #include "creaesercizio.h"
 
-DeepPtr<Esercizio> CreaEsercizio::createCardio(const string & n, const string & d, const uint m, const Orario & o, const uint v) const {
+DeepPtr<Esercizio> CreaEsercizio::createCardio(const string & n, const string & d, const uint m, const Orario & o, const uint v) {
     return DeepPtr<Esercizio>(new Cardio(n, d, m, o, v));
 }
 
-DeepPtr<Esercizio> CreaEsercizio::createSollevamentoPesi(const string & n, const string & d, const uint m, const uint p, const uint r, const uint s) const {
+DeepPtr<Esercizio> CreaEsercizio::createSollevamentoPesi(const string & n, const string & d, const uint m, const uint p, const uint r, const uint s) {
     return DeepPtr<Esercizio>(new SollevamentoPesi(n, d, m, p, r, s));
 }
 
-DeepPtr<Esercizio> CreaEsercizio::createCrossFit(const string & n, const string & d, const uint m, const Orario & o, const uint v, const uint p) const {
+DeepPtr<Esercizio> CreaEsercizio::createCrossFit(const string & n, const string & d, const uint m, const Orario & o, const uint v, const uint p) {
     return DeepPtr<Esercizio>(new CrossFit(n, d, m, o, v, p));
 }
 
-DeepPtr<Esercizio> CreaEsercizio::createRiposoPassivo(const string & n, const string & d, const string & ir, const Orario & o) const {
+DeepPtr<Esercizio> CreaEsercizio::createRiposoPassivo(const string & n, const string & d, const string & ir, const Orario & o) {
     return DeepPtr<Esercizio>(new RiposoPassivo(n, d, ir, o));
 }
 
-DeepPtr<Esercizio> CreaEsercizio::createRiposoAttivo(const string & ir, Esercizio * const e) const {
+DeepPtr<Esercizio> CreaEsercizio::createRiposoAttivo(const string & ir, Esercizio * const e) {
     return DeepPtr<Esercizio>(new RiposoAttivo(ir, e));
 }
--- Programmazione ad oggetti/ProgettoPaO/ProgettoPaO/creaesercizio.h
+++ Programmazione ad oggetti/ProgettoPaO/ProgettoPaO/creaesercizio.h
@@ -8,11 +8,11 @@
 
 class CreaEsercizio {
 public:
-    DeepPtr<Esercizio> createCardio(const string & n, const string & d, const uint m = 0, const Orario & o = 0, const uint v = 0) const;
-    DeepPtr<Esercizio> createSollevamentoPesi(const string & n, const string & d, const uint m = 0, const uint p = 0, const uint r = 0, const uint s = 0) const;
-    DeepPtr<Esercizio> createCrossFit(const string & n, const string & d, const uint m = 0, const Orario & o = 0, const uint v = 0, const uint p = 0) const;
-    DeepPtr<Esercizio> createRiposoPassivo(const string & n, const string & d, const string & ir,const Orario & o = 0) const;
-    DeepPtr<Esercizio> createRiposoAttivo(const string & ir, Esercizio * const e = 0) const;
+    static DeepPtr<Esercizio> createCardio(const string & n, const string & d, const uint m = 0, const Orario & o = 0, const uint v = 0);
+    static DeepPtr<Esercizio> createSollevamentoPesi(const string & n, const string & d, const uint m = 0, const uint p = 0, const uint r = 0, const uint s = 0);
+    static DeepPtr<Esercizio> createCrossFit(const string & n, const string & d, const uint m = 0, const Orario & o = 0, const uint v = 0, const uint p = 0);
+    static DeepPtr<Esercizio> createRiposoPassivo(const string & n, const string & d, const string & ir,const Orario & o = 0);
+    static DeepPtr<Esercizio> createRiposoAttivo(const string & ir, Esercizio * const e = 0);
 };
 
 #endif // CREAESERCIZIO_H
