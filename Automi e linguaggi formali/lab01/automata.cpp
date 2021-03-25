5d830c4201a3d289b884668039097b1c0ea3fe89
BrodieAdnan <adnangazi.ag@gmail.com>
2021-03-25 12:45:11 +0100

    Summary (required)

--- Automi e linguaggi formali/lab01/automata.cpp
+++ Automi e linguaggi formali/lab01/automata.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 /**
  * Constructor for Abstract DFA.
- * 
+ *
  * @param noStates
  *            Number of states in the DFA.
  */
-AbstractDFA::AbstractDFA(int noStates) {
-	// TODO: initialize data structures
+AbstractDFA::AbstractDFA(int noStates) : n_states(noStates), current_state(noStates > 0 ? 0 : -1), on_trap(false) {}
+
+void AbstractDFA::setFinalStates(const vector<int>& states) {
+	final_states = states;//final states contains values [0, n_states)
 }
+
+void AbstractDFA::addTransition(int from, int to, char c) {
+	transition[{from, c}] = to;
+}
+
 
 /**
  * Reset the automaton to the initial state.
  */
 void AbstractDFA::reset() {
-    // TODO: reset automaton to initial state
+	current_state = 0;
+	on_trap = false;
 }
 
 /**
@@ -25,38 +33,44 @@ void AbstractDFA::reset() {
  * for the given letter, then the automaton proceeds to the successor state.
  * Otherwise it goes to the sink state. By construction it will stay in the
  * sink for every input letter.
- * 
+ *
  * @param letter
  *            The current input.
  */
 void AbstractDFA::doStep(char letter) {
-    // TODO: do step by going to the next state according to the current
-    // state and the read letter.
+	if (!on_trap) {
+		if (transition.find({ current_state, letter }) != transition.end())
+			current_state = transition[{ current_state, letter }];
+		else
+		{
+			on_trap = true;
+			current_state = -1;
+		}
+	}
 }
 
 /**
  * Check if the automaton is currently accepting.
- * 
+ *
  * @return True, if the automaton is currently in the accepting state.
  */
 bool AbstractDFA::isAccepting() {
-    // TODO: return if the current state is accepting
-    return false;
+	return !on_trap && std::find(final_states.begin(), final_states.end(), current_state) != final_states.end();
 }
 
 /**
  * Run the DFA on the input.
- * 
+ *
  * @param inputWord
  *            stream that contains the input word
  * @return True, if if the word is accepted by this automaton
  */
-bool AbstractDFA::run(const string &inputWord) {
-    this->reset();
-    for(int i = 0; i < inputWord.length(); i++) {
-        doStep(inputWord[i]);
-    }
-    return isAccepting();
+bool AbstractDFA::run(const string& inputWord) {
+	this->reset();
+	for (int i = 0; i < inputWord.length(); i++) {
+		doStep(inputWord[i]);
+	}
+	return isAccepting();
 }
 
 
@@ -65,14 +79,16 @@ bool AbstractDFA::run(const string &inputWord) {
  * "foo" the constructed automaton looks like: -> () -f-> () -o-> () -o-> []
  * from every state (including the final one) every other input letter leads
  * to a distinguished sink state in which the automaton then remains
- * 
+ *
  * @param word
  *            A String that the automaton should recognize
  */
-WordDFA::WordDFA(const string &word) : AbstractDFA(0) {
-    // TODO: fill in correct number of states
-    
-    // TODO: build DFA recognizing the given word
+WordDFA::WordDFA(const string& word) : AbstractDFA(word.size()+1) {
+	setFinalStates({int(word.size())});
+
+	for (int i = 0; i < word.size(); i++) { 
+		addTransition(i, i + 1, word[i]); 
+	}
 }
 
 /**
@@ -81,20 +97,58 @@ WordDFA::WordDFA(const string &word) : AbstractDFA(0) {
  * with a newline, multiline comments that starts with (* and ends with *), and
  * multiline comments that starts with { and ends with }
  */
-CommentDFA::CommentDFA() : AbstractDFA(0) {
-    // TODO: fill in correct number of states
-    // TODO: build DFA recognizing comments
+CommentDFA::CommentDFA() : AbstractDFA(8) {
+	setFinalStates({ 3 });
+	
+	/* // comment */
+	addTransition(0, 1, '/');
+	addTransition(1, 2, '/');
+
+	for (char i = 0; i < 127; i++)
+	{
+		addTransition(2, 2, i);
+	}	
+
+	addTransition(2, 3, '\n');
+
+	/* { comment } */
+	addTransition(0, 4, '{');
+
+	for (char i = 0; i < 127; i++)
+	{
+		addTransition(4, 4, i);
+	}
+
+	addTransition(4, 3, '}');
+
+	/* (* comment *) */
+	addTransition(0, 5, '(');
+	addTransition(5, 6, '*');
+
+	for (char i = 0; i < 127; i++)
+	{
+		addTransition(6, 6, i);
+	}
+
+	addTransition(6, 7, '*');
+	
+	for (char i = 0; i < 127; i++)
+	{
+		addTransition(7, 6, i);
+	}
+	
+	addTransition(7, 3, ')');
 }
 
 /**
  * Performs one step of the DFA for a given letter. This method works
  * differently than in the superclass AbstractDFA.
- * 
+ *
  * @param letter
  *            The current input.
  */
 void CommentDFA::doStep(char letter) {
-    // TODO: implement accordingly
-}	
+	AbstractDFA::doStep(letter);
+}
 
 
--- Automi e linguaggi formali/lab01/automata.h
+++ Automi e linguaggi formali/lab01/automata.h
@@ -4,9 +4,18 @@
 #include<map>
 #include<vector>
 
+/*
+key [tpair] => value[int]
+
+stato:0 char: 'a' => stato: 3
+
+current_state=map[{0,'a'}]
+
+*/
+
 using namespace std;
 
-typedef std::pair<int,char> tpair;
+typedef std::pair<int, char> tpair;
 
 /**
  * Abstract class for Deterministic Finite Automata.
@@ -17,15 +26,31 @@ class AbstractDFA {
 	// - final states (and sink states)
 	// - transitions of the form (state, input) -> state
 	// - current state
-    
+private:
+	int n_states;
+	int current_state;
+	map<tpair, int> transition;
+	vector<int> final_states;
+	bool on_trap;
+
 public:
 	/**
 	 * Constructor for Abstract DFA.
-	 * 
+	 *
 	 * @param noStates
-	 *            Number of states in the DFA.
+	 *            Number of states in the DFA. 
 	 */
 	AbstractDFA(int noStates);
+
+	/**
+	 * Function for setting the final states
+	 */
+	void setFinalStates(const vector<int>& states);
+
+	/**
+	 * Function for setting a transition
+	 */
+	void addTransition(int from, int to, char c);
 
 	/**
 	 * Reset the automaton to the initial state.
@@ -37,27 +62,27 @@ public:
 	 * for the given letter, then the automaton proceeds to the successor state.
 	 * Otherwise it goes to the sink state. By construction it will stay in the
 	 * sink for every input letter.
-	 * 
+	 *
 	 * @param letter
 	 *            The current input.
 	 */
 	virtual void doStep(char letter);
-	
+
 	/**
 	 * Check if the automaton is currently accepting.
-	 * 
+	 *
 	 * @return True, if the automaton is currently in the accepting state.
 	 */
 	bool isAccepting();
 
 	/**
 	 * Run the DFA on the input.
-	 * 
+	 *
 	 * @param inputWord
 	 *            stream that contains the input word
 	 * @return True, if if the word is accepted by this automaton
 	 */
-	bool run(const string &inputWord);
+	bool run(const string& inputWord);
 };
 
 /**
@@ -71,11 +96,11 @@ public:
 	 * "foo" the constructed automaton looks like: -> () -f-> () -o-> () -o-> []
 	 * from every state (including the final one) every other input letter leads
 	 * to a distinguished sink state in which the automaton then remains
-	 * 
+	 *
 	 * @param word
 	 *            A String that the automaton should recognize
 	 */
-	 WordDFA(const string &word);
+	WordDFA(const string& word);
 };
 
 /**
@@ -86,21 +111,19 @@ class CommentDFA : public AbstractDFA {
 public:
 	/**
 	 * Construct a new DFA that recognizes comments within source code. There
-	 * are three kinds of comments: 
-	 *  1. a single line comment that starts with // and ends with a newline 
-	 *  2. a multiline comment that starts with (* and ends with *) 
+	 * are three kinds of comments:
+	 *  1. a single line comment that starts with // and ends with a newline
+	 *  2. a multiline comment that starts with (* and ends with *)
 	 *  3. a multiline comment that starts with { and ends with }
 	 */
 	CommentDFA();
-	
+
 	/**
 	 * Performs one step of the DFA for a given letter. This method works
 	 * differently than in the superclass AbstractDFA.
-	 * 
+	 *
 	 * @param letter
 	 *            The current input.
 	 */
-    virtual void doStep(char letter);
+	virtual void doStep(char letter);
 };
-
-
