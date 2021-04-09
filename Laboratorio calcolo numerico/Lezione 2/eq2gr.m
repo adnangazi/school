817eb0df648116eb81bbfe626a11ee0a2c463d61
BrodieAdnan <adnangazi.ag@gmail.com>
2021-04-09 12:53:57 +0200

    Lezione 2 calcolo

--- Laboratorio calcolo numerico/Lezione 2/eq2gr.m
+++ Laboratorio calcolo numerico/Lezione 2/eq2gr.m
@@ -1,8 +1,12 @@
+% impostazione formato
 format long;
+% titolo programma
 fprintf('Risuluzione eq. secondo grado\n');
+% inserimento dei coefficienti
 a = input('Inserire il valore di a: ');
 b = input('Inserire il valore di b: ');
 c = input('Inserire il valore di c: ');
+% controllo coefficienti validi
 if (a == 0 || b == 0 || c == 0)
     % messaggio di errore
     fprintf('Errore: i coefficienti non possono essere nulli!\n');
@@ -17,7 +21,7 @@ else
         % delta positivo
         fprintf('Delta positivo\n');
         delta = delta^(1/2);
-        % calcolo di x1 e x2
+        % calcolo di x1 e x2 e visualizzazione a video
         x1 = (-(b + delta)) / (2 * a);
         x2 = ((-b) + delta) / (2 * a);
         fprintf('Valore della radice x1: %f\n', x1);
@@ -26,6 +30,7 @@ else
         % delta uguale a zero
         fprintf('Delta uguale a zero\n');
         delta = delta^(1/2);
+        % calcolo di x (= x1 = x2) e visualizzazione a video
         x = (-b) / (2 * a);
         fprintf('Valore della radice: %f\n', x);
     end
