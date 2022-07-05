69edcea968ba5867a9719bdd77f400c67f50dbb1
iamadnangazi <adnangazi.ag@gmail.com>
2022-07-05 17:15:14 +0200

    Many updates

---
+++ university/bachelor/second-year/first-semester/programmazione-ad-oggetti/GoFlex/development/release/moc_controller.cpp
@@ -0,0 +1,244 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'controller.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../controller.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'controller.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Controller_t {
+    QByteArrayData data[31];
+    char stringdata0[527];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Controller_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Controller_t qt_meta_stringdata_Controller = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Controller"
+QT_MOC_LITERAL(1, 11, 9), // "addCardio"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 9), // "setCardio"
+QT_MOC_LITERAL(4, 32, 12), // "createCardio"
+QT_MOC_LITERAL(5, 45, 19), // "addSollevamentoPesi"
+QT_MOC_LITERAL(6, 65, 19), // "setSollevamentoPesi"
+QT_MOC_LITERAL(7, 85, 22), // "createSollevamentoPesi"
+QT_MOC_LITERAL(8, 108, 11), // "addCrossFit"
+QT_MOC_LITERAL(9, 120, 11), // "setCrossFit"
+QT_MOC_LITERAL(10, 132, 14), // "createCrossFit"
+QT_MOC_LITERAL(11, 147, 16), // "addRiposoPassivo"
+QT_MOC_LITERAL(12, 164, 16), // "setRiposoPassivo"
+QT_MOC_LITERAL(13, 181, 19), // "createRiposoPassivo"
+QT_MOC_LITERAL(14, 201, 21), // "addRiposoAttivoCardio"
+QT_MOC_LITERAL(15, 223, 21), // "setRiposoAttivoCardio"
+QT_MOC_LITERAL(16, 245, 24), // "createRiposoAttivoCardio"
+QT_MOC_LITERAL(17, 270, 31), // "addRiposoAttivoSollevamentoPesi"
+QT_MOC_LITERAL(18, 302, 31), // "setRiposoAttivoSollevamentoPesi"
+QT_MOC_LITERAL(19, 334, 34), // "createRiposoAttivoSollevament..."
+QT_MOC_LITERAL(20, 369, 23), // "addRiposoAttivoCrossFit"
+QT_MOC_LITERAL(21, 393, 23), // "setRiposoAttivoCrossFit"
+QT_MOC_LITERAL(22, 417, 26), // "createRiposoAttivoCrossFit"
+QT_MOC_LITERAL(23, 444, 6), // "remove"
+QT_MOC_LITERAL(24, 451, 9), // "whatIsMET"
+QT_MOC_LITERAL(25, 461, 14), // "whatIsCrossFit"
+QT_MOC_LITERAL(26, 476, 18), // "whatIsRiposoAttivo"
+QT_MOC_LITERAL(27, 495, 4), // "next"
+QT_MOC_LITERAL(28, 500, 8), // "previous"
+QT_MOC_LITERAL(29, 509, 9), // "goToStart"
+QT_MOC_LITERAL(30, 519, 7) // "goToEnd"
+
+    },
+    "Controller\0addCardio\0\0setCardio\0"
+    "createCardio\0addSollevamentoPesi\0"
+    "setSollevamentoPesi\0createSollevamentoPesi\0"
+    "addCrossFit\0setCrossFit\0createCrossFit\0"
+    "addRiposoPassivo\0setRiposoPassivo\0"
+    "createRiposoPassivo\0addRiposoAttivoCardio\0"
+    "setRiposoAttivoCardio\0createRiposoAttivoCardio\0"
+    "addRiposoAttivoSollevamentoPesi\0"
+    "setRiposoAttivoSollevamentoPesi\0"
+    "createRiposoAttivoSollevamentoPesi\0"
+    "addRiposoAttivoCrossFit\0setRiposoAttivoCrossFit\0"
+    "createRiposoAttivoCrossFit\0remove\0"
+    "whatIsMET\0whatIsCrossFit\0whatIsRiposoAttivo\0"
+    "next\0previous\0goToStart\0goToEnd"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Controller[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  159,    2, 0x0a /* Public */,
+       3,    0,  160,    2, 0x0a /* Public */,
+       4,    0,  161,    2, 0x0a /* Public */,
+       5,    0,  162,    2, 0x0a /* Public */,
+       6,    0,  163,    2, 0x0a /* Public */,
+       7,    0,  164,    2, 0x0a /* Public */,
+       8,    0,  165,    2, 0x0a /* Public */,
+       9,    0,  166,    2, 0x0a /* Public */,
+      10,    0,  167,    2, 0x0a /* Public */,
+      11,    0,  168,    2, 0x0a /* Public */,
+      12,    0,  169,    2, 0x0a /* Public */,
+      13,    0,  170,    2, 0x0a /* Public */,
+      14,    0,  171,    2, 0x0a /* Public */,
+      15,    0,  172,    2, 0x0a /* Public */,
+      16,    0,  173,    2, 0x0a /* Public */,
+      17,    0,  174,    2, 0x0a /* Public */,
+      18,    0,  175,    2, 0x0a /* Public */,
+      19,    0,  176,    2, 0x0a /* Public */,
+      20,    0,  177,    2, 0x0a /* Public */,
+      21,    0,  178,    2, 0x0a /* Public */,
+      22,    0,  179,    2, 0x0a /* Public */,
+      23,    0,  180,    2, 0x0a /* Public */,
+      24,    0,  181,    2, 0x0a /* Public */,
+      25,    0,  182,    2, 0x0a /* Public */,
+      26,    0,  183,    2, 0x0a /* Public */,
+      27,    0,  184,    2, 0x0a /* Public */,
+      28,    0,  185,    2, 0x0a /* Public */,
+      29,    0,  186,    2, 0x0a /* Public */,
+      30,    0,  187,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Controller::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Controller *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->addCardio(); break;
+        case 1: _t->setCardio(); break;
+        case 2: _t->createCardio(); break;
+        case 3: _t->addSollevamentoPesi(); break;
+        case 4: _t->setSollevamentoPesi(); break;
+        case 5: _t->createSollevamentoPesi(); break;
+        case 6: _t->addCrossFit(); break;
+        case 7: _t->setCrossFit(); break;
+        case 8: _t->createCrossFit(); break;
+        case 9: _t->addRiposoPassivo(); break;
+        case 10: _t->setRiposoPassivo(); break;
+        case 11: _t->createRiposoPassivo(); break;
+        case 12: _t->addRiposoAttivoCardio(); break;
+        case 13: _t->setRiposoAttivoCardio(); break;
+        case 14: _t->createRiposoAttivoCardio(); break;
+        case 15: _t->addRiposoAttivoSollevamentoPesi(); break;
+        case 16: _t->setRiposoAttivoSollevamentoPesi(); break;
+        case 17: _t->createRiposoAttivoSollevamentoPesi(); break;
+        case 18: _t->addRiposoAttivoCrossFit(); break;
+        case 19: _t->setRiposoAttivoCrossFit(); break;
+        case 20: _t->createRiposoAttivoCrossFit(); break;
+        case 21: _t->remove(); break;
+        case 22: _t->whatIsMET(); break;
+        case 23: _t->whatIsCrossFit(); break;
+        case 24: _t->whatIsRiposoAttivo(); break;
+        case 25: _t->next(); break;
+        case 26: _t->previous(); break;
+        case 27: _t->goToStart(); break;
+        case 28: _t->goToEnd(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject Controller::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Controller.data,
+    qt_meta_data_Controller,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Controller::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Controller::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Controller.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Controller::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 29;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
